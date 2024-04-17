e2ede057309b2becf1a34a0c39fa8ef4024b28ad
dellabeneta <m.dellabeneta@gmail.com>
2024-04-17 14:02:00 -0300

    testes

--- mongodb.tf
+++ mongodb.tf
@@ -7,15 +7,3 @@ resource "digitalocean_database_cluster" "database" {
   version              = var.database_version
   private_network_uuid = digitalocean_vpc.vpc.id
 }
-
-/*
-resource "digitalocean_database_user" "api_user" {
-  name       = var.database_api_user
-  cluster_id = digitalocean_database_cluster.database.id
-}
-
-resource "digitalocean_database_db" "api_db" {
-  name       = var.database_api_db
-  cluster_id = digitalocean_database_cluster.database.id
-}
-*/
